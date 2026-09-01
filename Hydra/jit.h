deac349b4a9d958351bc3ebc87fd13c9ad4bb4ae
AnerysRynz <anerysrynz@gmail.com>
2026-09-01 20:39:36 +0700

    fix: Add missing function declaration for hydra_jit_compile_hot_path in jit.h

--- Hydra/jit.h
+++ Hydra/jit.h
@@ -1,5 +1,9 @@
 #ifndef HYDRA_JIT_H
 #define HYDRA_JIT_H
+
 #include "zend.h"
+
 void hydra_jit_init(void);
-#endif
+bool hydra_jit_compile_hot_path(zend_op_array *op_array);
+
+#endif /* HYDRA_JIT_H */
